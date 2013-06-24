c2c6eda6763b70392d981e4d45f780509c53421f
cfinke <cfinke@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-24 02:45:38 +0000

    Initial commit.
    git-svn-id: https://plugins.svn.wordpress.org/inline-preview/trunk@730613 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inline-preview.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Plugin Name: Inline Preview
+Plugin URI: http://www.chrisfinke.com/wordpress/plugins/inline-preview
+Description: Adds a post preview next to the editor instead of in a new tab.
+Version: 3
+Author: Christopher Finke
+Author URI: http://www.chrisfinke.com/
+*/
+
+function inline_preview_enqueue( $hook ) {
+	if ( 'post-new.php' != $hook && 'post.php' != $hook )
+		return;
+
+	wp_enqueue_script( 'inline-preview', plugins_url( 'inline-preview/inline-preview.js', __FILE__ ), 'jquery', '3b1' );
+	wp_enqueue_style( 'inline-preview', plugins_url( 'inline-preview/inline-preview.css', __FILE__ ), array(), '3b1' );
+}
+
+add_action( 'admin_enqueue_scripts', 'inline_preview_enqueue' );
